6aab4e233f9ee0d3a13f252dfeb54b8f4b77caea
eduMalagutti <eduardosm2004@gmail.com>
2024-05-16 16:47:22 -0300

    Implementando post,put,delete

---
+++ src/main/java/com/eduardo/services/PersonServices.java
@@ -0,0 +1,74 @@
+package com.eduardo.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import com.eduardo.model.Person;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id) {
+        logger.info("finding one person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Leandro");
+        person.setLastName("Costa");
+        person.setAddress("Uberlandia - Minas Gerais - Brasil");
+        person.setGender("Male");
+
+        return person;
+    }
+
+    public List<Person> findAll() {
+        logger.info("finding all people!");
+
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
+    public Person create(Person person)
+    {
+        logger.info("Creating one person");
+
+        return person;
+    }
+    
+    public Person update(Person person)
+    {
+        logger.info("Updating one person");
+        //person.setFirstName(person.getFirstName() + "Updated");
+
+        return person;
+    }
+    
+    public void delete(String id)
+    {
+        logger.info("Deleting one person");
+        //person.setFirstName(person.getFirstName() + "Updated");
+    }
+
+    public Person mockPerson(int i)
+    {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name"+ i);
+        person.setLastName("Last name" + i);
+        person.setAddress("Some addres in Brasil" + i);
+        person.setGender("Male");
+        return person;
+    }
+}
