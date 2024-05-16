379bc9f4a5f9d3f0ba4e8ebe40ab8d1b53fe03bf
eduMalagutti <eduardosm2004@gmail.com>
2024-05-16 19:31:36 -0300

    Update PersonRepository.java

--- src/main/java/com/eduardo/repositories/PersonRepository.java
+++ src/main/java/com/eduardo/repositories/PersonRepository.java
@@ -1,9 +1,8 @@
 package com.eduardo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.eduardo.model.Person;
 
-@Repository
+//@Repository
 public interface PersonRepository extends JpaRepository<Person,Long> {}
